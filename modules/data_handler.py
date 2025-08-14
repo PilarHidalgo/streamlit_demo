f58644f68d2b8a48d0f8c8c7f5f0c53378a87b00
PilarHidalgo <37567214+PilarHidalgo@users.noreply.github.com>
2025-08-14 09:06:44 -0500

    Delete modules/data_handler.py

--- modules/data_handler.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def handle_dataset(path):
-    df = pd.read_csv(path)
-    df['age'] = df['age'].apply(lambda x: x + 1 if x > 18 else x)
-    df = df.dropna(subset=['age'])
-    return df
